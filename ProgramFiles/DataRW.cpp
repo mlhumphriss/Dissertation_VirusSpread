16ea04fd0d5003d2fc4ed11b89f50dfbb21dbfdc
mlhumphriss <91321666+mlhumphriss@users.noreply.github.com>
2025-05-04 02:34:13 +0100

    added datarw files and added some getters setters

---
+++ ProgramFiles/DataRW.cpp
@@ -0,0 +1,1 @@
+#include "DataRW.h"
--- ProgramFiles/Person.h
+++ ProgramFiles/Person.h
@@ -3,6 +3,15 @@
 class Person {
 public:
 
+	void setInfected(bool i, int d) {
+		infected = i;
+		dayInfected = d;
+	}
+
+	bool getInfected() {
+		return infected;
+	}
+
 protected:
 	bool	asymptomatic;
 	bool	infected;
