6636ed41f19e76ec646fc6d99758e6c3dbc5bcdf
Michael Humphriss <c1009080@newcastle.ac.uk>
2025-05-07 19:50:02 +0100

    added class initialisation method

--- ProgramFiles/Person.h
+++ ProgramFiles/Person.h
@@ -3,6 +3,20 @@
 class Person {
 public:
 
+	Person(int hR, int jI, float rebel, bool vac) {
+		this->healthRisk = hr;
+		this->jobImportance = jI;
+		this->rebeliousness = rebel;
+		this->asymptomatic = false;
+		this->dayInfected = 0;
+		this->timesInfected = 0;
+		this->infected = false;
+		this->vaccinated = vac;
+		this->daysSinceLeft = 0;
+	};
+
+	~Person() {}
+
 	void setInfected(bool i, int d) {
 		infected = i;
 		dayInfected = d; //Need an exception for if already infected before, or way to store multiple infections
@@ -16,8 +30,16 @@ public:
 		asymptomatic = a;
 	}
 
+	void setVaccinated(bool v) {
+		vaccinated = v;
+	}
+
 	int getDayInfected() {
 		return dayInfected;
+	}
+
+	int getTimesInfected() {
+		return timesInfected;
 	}
 
 	int getDaySinceLeft() {
@@ -32,10 +54,15 @@ public:
 		return asymptomatic;
 	}
 
+	bool getVaccinated() {
+		return vaccinated;
+	}
+
 protected:
 	bool	asymptomatic;
 	bool	infected;
 	int		dayInfected;
+	int		timesInfected;
 
 	bool	vaccinated;
 	int		healthRisk;
